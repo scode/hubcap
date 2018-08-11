56527928b4d7a01c4670840e0558c8876baddcbe
Peter Schuller <peter.schuller@infidyne.com>
2018-08-11 12:58:20 -0700

    Initial error-chain adoption. (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
 extern crate github_rs;
 extern crate serde_json;
 
+#[macro_use]
+extern crate error_chain;
+
+mod errors {
+    error_chain!{}
+}
+
+use errors::*;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(ref e) = run() {
+        use std::io::Write; // for write_fmt()
+        use error_chain::ChainedError; // for display_chain()
+        let stderr = &mut ::std::io::stderr();
+
+        writeln!(stderr, "{}", e.display_chain()).unwrap();
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<String> {
+    return Err("not yet implemented".into())
 }
