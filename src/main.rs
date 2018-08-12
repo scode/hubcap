65c6629fb797430feb0a3ebdaf4b26c7df7573cf
Peter Schuller <peter.schuller@infidyne.com>
2018-08-11 21:31:34 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,6 @@ use clap::SubCommand;
 use failure::Error;
 
 fn main() {
-    let matches = App::new("hubcap")
-        .version("0.1")
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .subcommand(SubCommand::with_name("status").about("print current branch and PR statuses"))
-        .get_matches();
-
     if let Err(ref e) = run() {
         use std::io::Write; // for write_fmt()
         let stderr = &mut ::std::io::stderr();
@@ -32,5 +21,21 @@ fn main() {
 }
 
 fn run() -> Result<(), Error> {
-    Err(format_err!("not yet implemented"))
+    let matches = App::new("hubcap")
+        .version("0.1")
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .subcommand(SubCommand::with_name("status").about("Print branch and PR status"))
+        .get_matches();
+
+    if let Some(_matches) = matches.subcommand_matches("status") {
+        Err(format_err!("not yet implemented"))
+    } else {
+        // TODO(scode): Print help when no args given.
+        Err(format_err!("no cmd given"))
+    }
 }
