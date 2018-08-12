76d527a4484d6b18aaea26cf6d13020f9027dbda
Peter Schuller <peter.schuller@infidyne.com>
2018-08-11 21:26:00 -0700

    clapwip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+extern crate clap;
 #[macro_use]
 extern crate failure;
 extern crate github_rs;
 extern crate serde_json;
 
+use clap::Arg;
+use clap::App;
+use clap::SubCommand;
 use failure::Error;
 
 fn main() {
+    let matches = App::new("hubcap")
+        .version("0.1")
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+        .subcommand(SubCommand::with_name("status")
+            .about("print current branch and PR statuses"))
+        .get_matches();
+
+
+
     if let Err(ref e) = run() {
         use std::io::Write; // for write_fmt()
         let stderr = &mut ::std::io::stderr();
