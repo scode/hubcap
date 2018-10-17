e96b54b5d204e25d988efb71ce73c50e5493b154
Peter Schuller <peter.schuller@infidyne.com>
2018-10-16 23:26:05 -0700

    Primordial git trait and implementation.

--- src/git.rs
+++ src/git.rs
@@ -1,3 +1,5 @@
+use failure::Error;
+use std::path::Path;
 use std::path::PathBuf;
 
 /// The possible states a file can be in, as reported by `git status`.
@@ -10,4 +12,44 @@ pub enum FileStatus {
     Renamed { new: PathBuf, old: PathBuf },
     Copied { new: PathBuf, old: PathBuf },
     UpdatedUnMerged(PathBuf),
+}
+
+pub trait Git {
+    /// Invoke `git status` and return the result.
+    ///
+    /// If the git working copy is clean, an empty vec is returned.
+    fn status(&self) -> Result<Vec<FileStatus>, Error>;
+}
+
+pub struct SystemGit {
+    /// The path of the git binary to execute.
+    git_path: PathBuf,
+
+    /// The path to the git repository; equivalent of the -C argument to git.
+    repo_path: PathBuf,
+}
+
+impl SystemGit {
+    pub fn new() -> SystemGit {
+        SystemGit {
+            git_path: PathBuf::from("git"),
+            repo_path: PathBuf::from("."),
+        }
+    }
+
+    pub fn git_path<'a>(&'a mut self, path: &Path) -> &'a mut SystemGit {
+        self.git_path = PathBuf::from(path);
+        self
+    }
+
+    pub fn repo_path<'a>(&'a mut self, path: &Path) -> &'a mut SystemGit {
+        self.repo_path = PathBuf::from(path);
+        self
+    }
+}
+
+impl Git for SystemGit {
+    fn status(&self) -> Result<Vec<FileStatus>, Error> {
+        Err(format_err!("not yet implemented"))
+    }
 }
